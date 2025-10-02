dcb2a19139e27d892c768416ab1fd0992f053c1d
Emir Karabeg <emirkarabeg@berkeley.edu>
2025-10-01 19:22:04 -0700

    feat(sidebar): created new sidebar with header and workflow list

--- apps/sim/app/workspace/[workspaceId]/w/[workflowId]/workflow.tsx
+++ apps/sim/app/workspace/[workspaceId]/w/[workflowId]/workflow.tsx
@@ -3,7 +3,6 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import ReactFlow, {
-  Background,
   ConnectionLineType,
   type Edge,
   type EdgeTypes,
@@ -1947,14 +1946,7 @@ const WorkflowContent = React.memo(() => {
             <Panel />
           </div>
           <ControlBar hasValidationErrors={nestedSubflowErrors.size > 0} />
-          <div className='workflow-container h-full'>
-            <Background
-              color='hsl(var(--workflow-dots))'
-              size={4}
-              gap={40}
-              style={{ backgroundColor: 'hsl(var(--workflow-background))' }}
-            />
-          </div>
+          <div className='workflow-container h-full' />
         </div>
       </div>
     )
@@ -2026,14 +2018,7 @@ const WorkflowContent = React.memo(() => {
           elevateNodesOnSelect={true}
           autoPanOnConnect={effectivePermissions.canEdit}
           autoPanOnNodeDrag={effectivePermissions.canEdit}
-        >
-          <Background
-            color='hsl(var(--workflow-dots))'
-            size={4}
-            gap={40}
-            style={{ backgroundColor: 'hsl(var(--workflow-background))' }}
-          />
-        </ReactFlow>
+        />
 
         {/* Show DiffControls if diff is available (regardless of current view mode) */}
         <DiffControls />
